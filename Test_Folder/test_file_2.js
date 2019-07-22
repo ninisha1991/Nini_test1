c3b9cd738ac353c0106a84f44b57bca420dfc893
Edgar Alfaro <39601988+edgarable@users.noreply.github.com>
2019-07-22 17:58:49 -0500

    Update test_file_2.js

--- Test_Folder/test_file_2.js
+++ Test_Folder/test_file_2.js
@@ -2,5 +2,6 @@ console.log("hello world");
 console.log("edgar commit")
 console.log("Welcome to rebase");
 console.log("second PR commit 1");
+console.log("To create conflict with master")
 console.log("Commit 1");
 
